d039480ecd727382e6d6a32ecad63964352ea69a
Emmanuel Odeke <emm.odeke@gmail.com>
2018-01-19 00:28:28 -0700

    reservations: service definitions
    Add some protobuf --> gRPC service definitions

--- reservations/defs.pb.go
+++ reservations/defs.pb.go
@@ -9,12 +9,19 @@ It is generated from these files:
 
 It has these top-level messages:
 	Reservation
+	Error
+	Reservations
 */
 package main
 
 import proto "github.com/golang/protobuf/proto"
 import fmt "fmt"
 import math "math"
+
+import (
+	context "golang.org/x/net/context"
+	grpc "google.golang.org/grpc"
+)
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ = proto.Marshal
@@ -33,6 +40,7 @@ type Reservation struct {
 	Code         string  `protobuf:"bytes,3,opt,name=Code" json:"Code,omitempty"`
 	Instructions string  `protobuf:"bytes,4,opt,name=Instructions" json:"Instructions,omitempty"`
 	Time         float64 `protobuf:"fixed64,5,opt,name=Time" json:"Time,omitempty"`
+	Error        *Error  `protobuf:"bytes,6,opt,name=Error" json:"Error,omitempty"`
 }
 
 func (m *Reservation) Reset()                    { *m = Reservation{} }
@@ -75,21 +83,250 @@ func (m *Reservation) GetTime() float64 {
 	return 0
 }
 
+func (m *Reservation) GetError() *Error {
+	if m != nil {
+		return m.Error
+	}
+	return nil
+}
+
+type Error struct {
+	Code    int32  `protobuf:"varint,1,opt,name=Code" json:"Code,omitempty"`
+	Message string `protobuf:"bytes,2,opt,name=Message" json:"Message,omitempty"`
+}
+
+func (m *Error) Reset()                    { *m = Error{} }
+func (m *Error) String() string            { return proto.CompactTextString(m) }
+func (*Error) ProtoMessage()               {}
+func (*Error) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{1} }
+
+func (m *Error) GetCode() int32 {
+	if m != nil {
+		return m.Code
+	}
+	return 0
+}
+
+func (m *Error) GetMessage() string {
+	if m != nil {
+		return m.Message
+	}
+	return ""
+}
+
+type Reservations struct {
+	Items []*Reservation `protobuf:"bytes,1,rep,name=Items" json:"Items,omitempty"`
+}
+
+func (m *Reservations) Reset()                    { *m = Reservations{} }
+func (m *Reservations) String() string            { return proto.CompactTextString(m) }
+func (*Reservations) ProtoMessage()               {}
+func (*Reservations) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{2} }
+
+func (m *Reservations) GetItems() []*Reservation {
+	if m != nil {
+		return m.Items
+	}
+	return nil
+}
+
 func init() {
 	proto.RegisterType((*Reservation)(nil), "main.Reservation")
+	proto.RegisterType((*Error)(nil), "main.Error")
+	proto.RegisterType((*Reservations)(nil), "main.Reservations")
+}
+
+// Reference imports to suppress errors if they are not otherwise used.
+var _ context.Context
+var _ grpc.ClientConn
+
+// This is a compile-time assertion to ensure that this generated file
+// is compatible with the grpc package it is being compiled against.
+const _ = grpc.SupportPackageIsVersion4
+
+// Client API for App service
+
+type AppClient interface {
+	Delete(ctx context.Context, in *Reservation, opts ...grpc.CallOption) (*Error, error)
+	FindByCode(ctx context.Context, in *Reservation, opts ...grpc.CallOption) (*Reservation, error)
+	FindByEmail(ctx context.Context, in *Reservation, opts ...grpc.CallOption) (*Reservations, error)
+	Create(ctx context.Context, in *Reservation, opts ...grpc.CallOption) (*Reservation, error)
+}
+
+type appClient struct {
+	cc *grpc.ClientConn
+}
+
+func NewAppClient(cc *grpc.ClientConn) AppClient {
+	return &appClient{cc}
+}
+
+func (c *appClient) Delete(ctx context.Context, in *Reservation, opts ...grpc.CallOption) (*Error, error) {
+	out := new(Error)
+	err := grpc.Invoke(ctx, "/main.App/Delete", in, out, c.cc, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *appClient) FindByCode(ctx context.Context, in *Reservation, opts ...grpc.CallOption) (*Reservation, error) {
+	out := new(Reservation)
+	err := grpc.Invoke(ctx, "/main.App/FindByCode", in, out, c.cc, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *appClient) FindByEmail(ctx context.Context, in *Reservation, opts ...grpc.CallOption) (*Reservations, error) {
+	out := new(Reservations)
+	err := grpc.Invoke(ctx, "/main.App/FindByEmail", in, out, c.cc, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *appClient) Create(ctx context.Context, in *Reservation, opts ...grpc.CallOption) (*Reservation, error) {
+	out := new(Reservation)
+	err := grpc.Invoke(ctx, "/main.App/Create", in, out, c.cc, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// Server API for App service
+
+type AppServer interface {
+	Delete(context.Context, *Reservation) (*Error, error)
+	FindByCode(context.Context, *Reservation) (*Reservation, error)
+	FindByEmail(context.Context, *Reservation) (*Reservations, error)
+	Create(context.Context, *Reservation) (*Reservation, error)
+}
+
+func RegisterAppServer(s *grpc.Server, srv AppServer) {
+	s.RegisterService(&_App_serviceDesc, srv)
+}
+
+func _App_Delete_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Reservation)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(AppServer).Delete(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/main.App/Delete",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(AppServer).Delete(ctx, req.(*Reservation))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _App_FindByCode_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Reservation)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(AppServer).FindByCode(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/main.App/FindByCode",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(AppServer).FindByCode(ctx, req.(*Reservation))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _App_FindByEmail_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Reservation)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(AppServer).FindByEmail(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/main.App/FindByEmail",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(AppServer).FindByEmail(ctx, req.(*Reservation))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _App_Create_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Reservation)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(AppServer).Create(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/main.App/Create",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(AppServer).Create(ctx, req.(*Reservation))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+var _App_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "main.App",
+	HandlerType: (*AppServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "Delete",
+			Handler:    _App_Delete_Handler,
+		},
+		{
+			MethodName: "FindByCode",
+			Handler:    _App_FindByCode_Handler,
+		},
+		{
+			MethodName: "FindByEmail",
+			Handler:    _App_FindByEmail_Handler,
+		},
+		{
+			MethodName: "Create",
+			Handler:    _App_Create_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "defs.proto",
 }
 
 func init() { proto.RegisterFile("defs.proto", fileDescriptor0) }
 
 var fileDescriptor0 = []byte{
-	// 142 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0x4a, 0x49, 0x4d, 0x2b,
-	0xd6, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0xc9, 0x4d, 0xcc, 0xcc, 0x53, 0x6a, 0x65, 0xe4,
-	0xe2, 0x0e, 0x4a, 0x2d, 0x4e, 0x2d, 0x2a, 0x4b, 0x2c, 0xc9, 0xcc, 0xcf, 0x13, 0x12, 0xe1, 0x62,
-	0x75, 0xcd, 0x4d, 0xcc, 0xcc, 0x91, 0x60, 0x54, 0x60, 0xd4, 0xe0, 0x0c, 0x82, 0x70, 0x40, 0xa2,
-	0x61, 0xa9, 0x79, 0xa5, 0xa9, 0x12, 0x4c, 0x10, 0x51, 0x30, 0x47, 0x48, 0x88, 0x8b, 0xc5, 0x39,
-	0x3f, 0x25, 0x55, 0x82, 0x19, 0x2c, 0x08, 0x66, 0x0b, 0x29, 0x71, 0xf1, 0x78, 0xe6, 0x15, 0x97,
-	0x14, 0x95, 0x26, 0x83, 0x8c, 0x2b, 0x96, 0x60, 0x01, 0xcb, 0xa1, 0x88, 0x81, 0xf4, 0x85, 0x64,
-	0xe6, 0xa6, 0x4a, 0xb0, 0x2a, 0x30, 0x6a, 0x30, 0x06, 0x81, 0xd9, 0x49, 0x6c, 0x60, 0x47, 0x19,
-	0x03, 0x02, 0x00, 0x00, 0xff, 0xff, 0x27, 0x19, 0x55, 0xa3, 0xa2, 0x00, 0x00, 0x00,
+	// 285 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x91, 0xbf, 0x4e, 0xc3, 0x30,
+	0x10, 0xc6, 0x63, 0xf2, 0x07, 0x71, 0xe9, 0xc2, 0x89, 0xc1, 0x62, 0x0a, 0x5e, 0x88, 0x18, 0x22,
+	0x14, 0xfe, 0xcd, 0x50, 0x8a, 0xd4, 0x81, 0x25, 0x42, 0xec, 0x81, 0x1c, 0x28, 0x52, 0x13, 0x47,
+	0xb6, 0x8b, 0xc4, 0x1b, 0xf1, 0x3a, 0xbc, 0x11, 0xb2, 0xdd, 0xa2, 0xa0, 0x74, 0x60, 0xbb, 0xfb,
+	0xdd, 0x77, 0xfe, 0x3e, 0xdb, 0x00, 0x0d, 0xbd, 0xe9, 0x62, 0x50, 0xd2, 0x48, 0x8c, 0xba, 0xba,
+	0xed, 0xc5, 0x17, 0x83, 0xb4, 0x22, 0x4d, 0xea, 0xa3, 0x36, 0xad, 0xec, 0xf1, 0x08, 0xe2, 0x45,
+	0x57, 0xb7, 0x2b, 0xce, 0x32, 0x96, 0x1f, 0x54, 0xbe, 0xb1, 0xf4, 0x99, 0xfa, 0x35, 0xf1, 0x3d,
+	0x4f, 0x5d, 0x83, 0x08, 0xd1, 0x5c, 0x36, 0xc4, 0x43, 0x07, 0x5d, 0x8d, 0x02, 0x66, 0xcb, 0x5e,
+	0x1b, 0xb5, 0x7e, 0xb5, 0xc7, 0x69, 0x1e, 0xb9, 0xd9, 0x1f, 0x66, 0xf7, 0x9e, 0xda, 0x8e, 0x78,
+	0x9c, 0xb1, 0x9c, 0x55, 0xae, 0xc6, 0x13, 0x88, 0x17, 0x4a, 0x49, 0xc5, 0x93, 0x8c, 0xe5, 0x69,
+	0x99, 0x16, 0x36, 0x5d, 0xe1, 0x50, 0xe5, 0x27, 0xe2, 0x6a, 0x23, 0xf9, 0xf5, 0xb5, 0x11, 0xe3,
+	0x8d, 0x2f, 0x87, 0xfd, 0x47, 0xd2, 0xba, 0x7e, 0xdf, 0x66, 0xdc, 0xb6, 0xe2, 0x06, 0x66, 0xa3,
+	0x0b, 0x6a, 0x3c, 0x85, 0x78, 0x69, 0xa8, 0xd3, 0x9c, 0x65, 0x61, 0x9e, 0x96, 0x87, 0xde, 0x69,
+	0x24, 0xa9, 0xfc, 0xbc, 0xfc, 0x66, 0x10, 0xde, 0x0e, 0x03, 0x9e, 0x41, 0x72, 0x4f, 0x2b, 0x32,
+	0x84, 0x53, 0xed, 0xf1, 0x38, 0xa8, 0x08, 0xf0, 0x12, 0xe0, 0xa1, 0xed, 0x9b, 0xbb, 0x4f, 0x17,
+	0x6a, 0x87, 0x7e, 0x8a, 0x44, 0x80, 0xd7, 0x90, 0xfa, 0x2d, 0xff, 0xda, 0x3b, 0xd6, 0x70, 0x82,
+	0xb4, 0x08, 0xf0, 0x1c, 0x92, 0xb9, 0xa2, 0xda, 0xfc, 0xdb, 0xe9, 0x25, 0x71, 0x7f, 0x7f, 0xf1,
+	0x13, 0x00, 0x00, 0xff, 0xff, 0xb9, 0x00, 0x78, 0xad, 0x09, 0x02, 0x00, 0x00,
 }
